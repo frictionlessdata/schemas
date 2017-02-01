800b1d847e588dcfcdcf684f1ec1ff8a17b79300
roll <roll@users.noreply.github.com>
2017-02-01 12:40:12 +0300

    Updated registry.json urls using https (#67)

--- tests/test_registry_json.py
+++ tests/test_registry_json.py
@@ -2,7 +2,7 @@ import io
 import os
 import json
 import unittest
-import urllib.request
+import requests
 
 BASE_PATH = os.path.abspath(
     os.path.join(
@@ -54,26 +54,22 @@ class TestRegistryJson(unittest.TestCase):
             assert os.path.isfile(path), msg
 
     def test_schema_urls_exist(self):
-        is_successful = lambda req: req.status >= 200 and req.status < 400
-        is_redirect = lambda req: req.status >= 300 and req.status < 400
         registry = json.load(io.open(REGISTRY_PATH, encoding='utf-8'))
         for id, item in registry.items():
             url = item['schema']
-            res = self._make_head_request(url)
+            res = requests.head(url)
             msg = "Error fetching schema_url '{0}' of schema '{1}'"
             msg = msg.format(url, id)
-            assert (is_successful(res) or is_redirect(res)), msg
+            assert res.ok, msg
 
     def test_specification_urls_exist(self):
-        is_successful = lambda req: req.status >= 200 and req.status < 400
-        is_redirect = lambda req: req.status >= 300 and req.status < 400
         registry = json.load(io.open(REGISTRY_PATH, encoding='utf-8'))
         for id, item in registry.items():
             url = item['schema']
-            res = self._make_head_request(url)
+            res = requests.head(url)
             msg = "Error fetching specification '{0}' of schema '{1}'"
             msg = msg.format(url, id)
-            assert (is_successful(res) or is_redirect(res)), msg
+            assert res.ok, msg
 
     def test_main_schemas_present(self):
         registry = json.load(io.open(REGISTRY_PATH, encoding='utf-8'))
@@ -81,7 +77,3 @@ class TestRegistryJson(unittest.TestCase):
         self.assertIn('datapackage', ids)
         self.assertIn('tabular-datapackage', ids)
         self.assertIn('fiscal-datapackage', ids)
-
-    def _make_head_request(self, url):
-        req = urllib.request.Request(url, method='HEAD')
-        return urllib.request.urlopen(req)
